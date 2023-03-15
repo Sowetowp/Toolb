acc71174e57e233618ac9b4f553c04d3cc5297cb
sowetowp <ayodejiamzat@gmil.com>
2023-03-15 22:24:07 +0100

    gh

--- controllers/ToolsController.js
+++ controllers/ToolsController.js
@@ -65,7 +65,7 @@ export const update_tool = asyncHandler(async(req, res) => {
         tool.availability = availability || tool.availability
         
 
-        const updatedtool = await tool.save()
+        const updatedtool = await tool.save({suppressWarning: true})
 
         if(updatedtool){
             res.status(201).json({
